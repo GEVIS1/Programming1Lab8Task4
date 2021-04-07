ad67ed09d5c71c9fded1e360eb49b140466f46a7
GEVIS1 <GEVIS1@student.op.ac.nz>
2021-04-08 09:47:59 +1200

    Added comments

--- Programming1Lab8Task4/Program.cs
+++ Programming1Lab8Task4/Program.cs
@@ -11,28 +11,36 @@ namespace Programming1Lab8Task4
                               "The Skier will try to get 2000 metres down the slope before the Abominable Snow Monster catches him and\n" +
                               "he becomes tea for the monster.\n\n");
 
+            // Declare variables and objects
             Random rand = new Random();
             int skierDistance, absDistance, abs;
             skierDistance = absDistance = abs = 0;
 
+            // Initialize variables
             skierDistance = rand.Next(20);
             abs = rand.Next(10);
             absDistance = skierDistance + abs;
 
+            // Report starting conditions
             ReportDistances(skierDistance, absDistance);
             
+            // While the skier hasn't gone 2000 metres down the field and the abominable snow monster hasn't caught up, keep looping
             while(skierDistance < 2000 && (absDistance > skierDistance))
             {
+                // Update distances for both skier and monster and report
                 skierDistance += rand.Next(5);
                 absDistance += rand.Next(5);
                 ReportDistances(skierDistance, absDistance);
             }
             
+            // Check if the skier's win condition was met
             if(skierDistance >= 2000)
             {
                 Console.WriteLine("\n\nThe skier made it and got away scot free! The Abominable Snow Monster stays hungry tonight.");
             }
 
+
+            // Otherwise there is a big bloodspot on the ski slope
             else
             {
                 Console.WriteLine("\n\nSomeone call the ambulance! Or maybe the morgue..\n" +
@@ -43,6 +51,7 @@ namespace Programming1Lab8Task4
 
             Console.ReadLine();
         }
+
 
         static void ReportDistances(int skier, int abs)
         {
